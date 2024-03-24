c8439f73ac8689d7b92e580df7c1b6115ec88d4a
Code-Parth <softsparths@gmail.com>
2024-03-24 09:19:39 +0530

    Refactor code to improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
-export default function Home() {
+"use client";
+
+import { useEffect, useState } from "react";
+import fetchTokens from "../lib/searchAssets";
+
+export default function Home(req: any, res: any) {
+    const [tokens, setTokens] = useState<any>(null);
+
+    useEffect(() => {
+        fetchTokens("7KFerXQA71zx5nLGiqFz6mcDTWBzYAoAWXf9EkRbGx8u").then(setTokens).catch(console.error);
+    }, []);
+
     return (
-        <>
-        </>
+        <div>
+            <h1>Search Assets</h1>
+            <pre>{JSON.stringify(tokens, null, 2)}</pre>
+        </div>
     );
+
 }

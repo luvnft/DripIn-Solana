51731e7bc37e5bd6fc6b99555662b3eeac5642cb
Code-Parth <softsparths@gmail.com>
2024-03-24 15:46:41 +0530

    Add Image component and update Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import fetchTokens from "@/lib/searchAssets";
 import { Label } from "@/components/ui/label";
 import ItemsResponse from "@/Types/SearchAssetsTypes";
 import { useWallet } from "@solana/wallet-adapter-react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 
 export default function Home() {
@@ -26,7 +27,7 @@ export default function Home() {
 
     return (
         <>
-            <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-4">
+            <div className="grid grid-cols-2 max-sm:grid-cols-1 gap-4 py-4">
                 <Card>
                     <CardHeader>
                         <CardTitle>Total NFTs</CardTitle>
@@ -45,7 +46,22 @@ export default function Home() {
                 </Card>
             </div>
 
-            <pre>{JSON.stringify(tokens, null, 2)}</pre>
+            <div className="grid grid-cols-4 gap-4">
+                {tokens?.items.items.map((item, index) => (
+                    <Card key={index}>
+                        <CardHeader>
+                            <CardTitle>{item.content.metadata.name}</CardTitle>
+                            <CardDescription>{item.grouping[0].collection_metadata.name}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            {/* eslint-disable-next-line @next/next/no-img-element */}
+                            <img src={item.content.files[0].uri} width={256} height={256} alt={item.content.metadata.name} className="aspect-square w-full h-full rounded-md" />
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+
+            {/* <pre>{JSON.stringify(tokens, null, 2)}</pre> */}
         </>
     );
 }

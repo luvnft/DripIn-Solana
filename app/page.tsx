0aa62c17c89a4996c188e8f350d56b66a183ea04
Code-Parth <softsparths@gmail.com>
2024-03-24 13:41:23 +0530

    format code

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,16 @@ import { useEffect, useState } from "react";
 import fetchTokens from "@/lib/searchAssets";
 import { useWallet } from "@solana/wallet-adapter-react";
 
-export default function Home(req: any, res: any) {
+export default function Home() {
     const [tokens, setTokens] = useState<any>(null);
     const { publicKey } = useWallet();
 
     useEffect(() => {
-        if (publicKey){
-            const walletAddress = publicKey.toBase58(); // Convert the public key to a base58 string
-            fetchTokens(walletAddress)
-                .then(setTokens)
-                .catch(console.error); 
+        if (publicKey) {
+            const walletAddress = publicKey.toBase58();
+            fetchTokens(walletAddress).then(setTokens).catch(console.error);
         }
-        else{
+        else {
             setTokens(null);
         }
     }, [publicKey])

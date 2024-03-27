2e9b48120af7994f14e5e360dc6f407e133187dd
Code-Parth <softsparths@gmail.com>
2024-03-27 18:55:12 +0530

    Update fallback loading message in Room component

--- app/Room.tsx
+++ app/Room.tsx
@@ -7,7 +7,7 @@ import { ClientSideSuspense } from "@liveblocks/react";
 export function Room({ children, roomId }: { children: ReactNode, roomId: string}) {
     return (
         <RoomProvider id={roomId} initialPresence={{}}>
-            <ClientSideSuspense fallback={<div>Loading…</div>}>
+            <ClientSideSuspense fallback={<div className="pt-6">Loading…</div>}>
                 {() => children}
             </ClientSideSuspense>
         </RoomProvider>

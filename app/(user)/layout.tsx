df7d6b8966401787a8a284a1342867992d92c286
Code-Parth <softsparths@gmail.com>
2024-04-03 13:16:03 +0530

    Add useEffect hook to redirect to home page if publicKey is null

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import "@/app/globals.css";
 import "@/style/SolanaWallet.css";
 import { Sora } from "next/font/google";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useWallet } from "@solana/wallet-adapter-react";
 
@@ -17,10 +18,11 @@ export default function RootLayout({
     const router = useRouter();
     const { publicKey } = useWallet();
 
-    if (!publicKey) {
-        router.push("/");
-        return null;
-    }
+    useEffect(() => {
+        if (!publicKey) {
+            router.push("/");
+        }
+    }, [publicKey, router]);
 
     return (
         <div className={`${sora.className} flex flex-col min-h-screen`}>
